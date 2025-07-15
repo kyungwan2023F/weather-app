c00e393a4e7af3b92a28f872a17f378bf0666ac5
John Doe <149332198+kyungwan2023F@users.noreply.github.com>
2025-07-15 07:22:48 -0400

    Include dist folder for GitHub Pages deployment

---
+++ dist/src_assets_icons_thunder-showers-night_svg.bundle.js
@@ -0,0 +1,22 @@
+"use strict";
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(self["webpackChunkweatherapp"] = self["webpackChunkweatherapp"] || []).push([["src_assets_icons_thunder-showers-night_svg"],{
+
+/***/ "./src/assets/icons/thunder-showers-night.svg":
+/*!****************************************************!*\
+  !*** ./src/assets/icons/thunder-showers-night.svg ***!
+  \****************************************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("module.exports = __webpack_require__.p + \"e83e5d078be84eac9071.svg\";\n\n//# sourceURL=webpack://weatherapp/./src/assets/icons/thunder-showers-night.svg?");
+
+/***/ })
+
+}]);
